d79885f059ce81b17b23ce530142988d8e3c3d09
s-yasuoka <migayasu@gmail.com>
2022-08-15 19:01:51 +0900

    fin

--- Main.java
+++ Main.java
@@ -1,4 +1,15 @@
 public class Main{
   public static void main(String[] args){
+    introduceOneself();
+  }
+  public static void introduceOneself(){
+    String name = "田中";
+    int age = 27;
+    double heigth = 180.0;
+    char zodiac = '猪';
+    System.out.println("私の名前は"+name+"です。");
+    System.out.println("年齢は"+age+"歳です。");
+    System.out.println("身長は"+heigth+"cmです。");
+    System.out.println("十二支は"+zodiac+"です。");
   }
 }
