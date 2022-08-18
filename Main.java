37355a70b99ad7d3445a445e381bff75caa2c875
s-yasuoka <migayasu@gmail.com>
2022-08-18 22:17:06 +0900

    fin

--- Main.java
+++ Main.java
@@ -3,11 +3,17 @@ public class Main{
     String title = "定期連絡";
     String address = "aa@a";
     String text = "明日は雨です。";
-    email(title, address, text);
+    email(address, text);
   }
   public static void email(String title, String address, String text){
     System.out.println(address + "に以下のメールを送信しました。");
     System.out.println("件名:"+title);
+    System.out.println("本文:"+text);
+  }
+
+  public static void email(String address, String text){
+    System.out.println(address + "に以下のメールを送信しました。");
+    System.out.println("件名:無題");
     System.out.println("本文:"+text);
   }
 }
