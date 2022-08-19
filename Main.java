07f3dcf051506cc0400c976addecee57815b193d
s-yasuoka <migayasu@gmail.com>
2022-08-19 15:25:00 +0900

    fin

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main{
   public static void main(String[] args){
     String title = "定期連絡";
     String address = "aa@a";
-    String text = "明日は雨です。";
+    String text = "明日は曇りsです。";
     email(address, text);
   }
   public static void email(String title, String address, String text){
