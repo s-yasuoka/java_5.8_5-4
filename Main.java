07de36addf38b9b75c748a50269cd8e180d0bb30
s-yasuoka <migayasu@gmail.com>
2022-08-20 13:57:50 +0900

    first commit

--- Main.java
+++ Main.java
@@ -5,15 +5,4 @@ public class Main{
     String text = "明日は曇りsです。";
     email(address, text);
   }
-  public static void email(String title, String address, String text){
-    System.out.println(address + "に以下のメールを送信しました。");
-    System.out.println("件名:"+title);
-    System.out.println("本文:"+text);
-  }
-
-  public static void email(String address, String text){
-    System.out.println(address + "に以下のメールを送信しました。");
-    System.out.println("件名:無題");
-    System.out.println("本文:"+text);
-  }
 }
