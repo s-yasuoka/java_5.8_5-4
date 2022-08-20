2cddb181459e5c3d768013ddcf9aa98066237643
s-yasuoka <migayasu@gmail.com>
2022-08-20 14:12:04 +0900

    first commit

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 public class Main{
   public static void main(String[] args){
-    String title = "定期連絡";
-    String address = "aa@a";
-    String text = "明日は曇りsです。";
-    email(address, text);
+    
   }
+  public static void calcTriangleArea(double bottom, double height){
+
+  }
+  public static void calcCirclrArea(double radius){
+    
+  }
+
 }
