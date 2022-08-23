c0dc222db6f1a995f87562cce284f7e92767f399
s-yasuoka <migayasu@gmail.com>
2022-08-23 21:43:54 +0900

    fin

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main{
   public static void main(String[] args){
-    double triangleArea = calcTriangleArea(10.0, 5.0);
+    double triangleArea = calcTriangleArea(10.0, 6.0);
     System.out.println("三角形の面積:" + triangleArea + "㎠");
 
     double circlrArea = calcCirclrArea(5.0);
